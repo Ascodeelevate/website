a9f9195d890cbee029e357ea9fec3562aff3c553
sheriffjimoh <sheriffjimoh88@gmail.com>
2025-01-21 14:49:15 +0100

    fix: redesign header and hero sections

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -10,6 +10,7 @@ export function AppHeader() {
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
+
   };
 
   const navItems = useMemo(() => [
@@ -23,19 +24,19 @@ export function AppHeader() {
   const isActive = (href: string) => pathname === href;
 
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <nav className="bg-primary-400 sticky z-10 top-0 border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link href="/" className="flex items-center space-x-1 rtl:space-x-reverse">
           <OptimizedImage src="/icon.png"  width={50} height={50} alt="Ascodeelevate Logo" />
           <div className="flex flex-col justify-start items-start">
-            <span className="text-lg font-semibold text-black">AS CodeElevate</span>
-            <span className="text-xs font-medium text-primary-600 dark:text-gray-400">Solution</span>
+            <span className="text-lg font-semibold text-white">AS CodeElevate</span>
+            <span className="text-xs font-medium text-black hover:text-primary-600 dark:text-gray-400">Solution</span>
           </div>
         </Link>
         <button
           onClick={toggleMenu}
           type="button"
-          className="inline-flex items-center p-2 border-0 bg-white w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 border-0 bg-white w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-default"
           aria-expanded={isOpen ? 'true' : 'false'}
         >
@@ -44,13 +45,14 @@ export function AppHeader() {
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
           </svg>
         </button>
-        <div className={`${isOpen ? 'block' : 'hidden'} w-full md:flex md:items-center md:space-x-40 md:w-auto`} id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <div className={`${isOpen ? 'block' : 'hidden'} w-full lg:flex lg:items-center lg:space-x-40 lg:w-auto`} id="navbar-default">
+          <ul className="font-medium flex flex-col p-4 lg:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 lg:flex-row lg:space-x-8 rtl:space-x-reverse lg:mt-0 lg:border-0 lg:bg-white dark:bg-gray-800 lg:dark:bg-gray-900 dark:border-gray-700">
             {navItems.map((item) => (
               <li key={item.href}>
                 <Link
+                 onClick={toggleMenu}
                   href={item.href}
-                  className={`block py-2 px-3 rounded ${isActive(item.href) ? 'text-primary-600' : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary-600 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'}`}
+                  className={`block py-2 px-3 rounded ${isActive(item.href) ? 'text-primary-600' : 'text-gray-900 hover:bg-gray-100 lg:hover:bg-transparent lg:hover:text-primary-600 dark:text-white lg:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent'}`}
                 >
                   {item.name}
                 </Link>
@@ -58,10 +60,10 @@ export function AppHeader() {
             ))}
           </ul>
 
-          <div className="mt-4 md:mt-0 md:ml-4">
+          <div className="mt-4 lg:mt-0 lg:ml-4">
             <Link
               href="/contact"
-              className="block w-full py-2 px-3 text-center text-secondary-600 bg-primary-600 rounded md:inline-block md:w-auto md:px-5 md:py-2 md:ml-2 hover:bg-secondary-600  hover:text-primary-500"
+              className="block w-full py-2 px-3 text-center text-black bg-white rounded lg:inline-block lg:w-auto lg:px-5 lg:py-2 lg:ml-2 hover:text-primary-600"
             >
               Get started
             </Link>
